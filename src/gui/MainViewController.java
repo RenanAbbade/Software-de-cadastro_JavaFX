03e2eb2157cd823847d82d9bcd5ad14d537a1461
RenanAbbade <renanabbade1997@gmail.com>
2019-07-16 12:09:02 -0300

    About Adicionada

---
+++ src/gui/MainViewController.java
@@ -0,0 +1,71 @@
+package gui;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import application.Main;
+import gui.util.Alerts;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
+
+public class MainViewController implements Initializable {
+	// ID'S
+	@FXML
+	private MenuItem menuItemSeller;
+
+	@FXML
+	private MenuItem menuItemDepartment;
+
+	@FXML
+	private MenuItem menuItemAbout;
+
+	// ACOES
+	@FXML
+	public void onMenuItemSellerAction() {
+		System.out.println("Vendedor");
+	}
+
+	@FXML
+	public void onMenuItemDepartmentAction() {
+		System.out.println("Departamento");
+	}
+
+	@FXML
+	public void onMenuItemAboutAction() {
+		loadView("/gui/About.fxml"); // Tratamento para o Main abrir o about no método Initialize
+	}
+
+	@Override
+	public void initialize(URL uri, ResourceBundle rb) {
+
+	}
+
+	private synchronized void loadView(String absoluteName) {//syncronized para não ser interrompida pela aplicação MultiThread
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			VBox newVBox = loader.load();
+			
+			Scene mainScene = Main.getMainScene();
+			//Pegando ref da tela original para a tela About
+			VBox mainVBox = (VBox)((ScrollPane) mainScene.getRoot()).getContent();//Este metodo pega o primeiro elemento da minha View
+			
+			Node mainMenu = mainVBox.getChildren().get(0);
+			mainVBox.getChildren().clear();
+			mainVBox.getChildren().add(mainMenu);
+			mainVBox.getChildren().addAll(newVBox.getChildren());
+			
+			
+		}catch (IOException e) {
+			Alerts.showAlert("IOException", null, "error:"+e.getMessage(), AlertType.ERROR);
+		}
+	}
+
+}
